7b38150181e8ec591b78e030547521c81761c755
Christina Zhivilova <krishakirova@yandex.ru>
2022-10-21 18:07:13 +0300

    Добавление Task 34

---
+++ Task34/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет 
+//количество чётных чисел в массиве.
+
+
+int[] CountEqual = new int[8];
+
+void PrintArray(int[] array)
+{
+    int temp = array.Length;
+    for (int i = 0; i < temp; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int PrintNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine();
+PrintArray(CountEqual);
+Console.WriteLine();
+int res = PrintNumbers(CountEqual);
+Console.WriteLine($"Чётных чисел в массиве = {res}");
---
+++ Task36/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+Console.Clear();
+Console.WriteLine("Введите массив через пробел: ");
+string input = Console.ReadLine();
+int[] numArray = GetArrayFromString(input);
+Console.WriteLine("Введите искомое число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (CheckArray(numArray, n))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+
+}
+
+Console.Clear();
+Console.WriteLine(" Hello ");
+int[] CountEqual = new int[8];
+int count = 0;
+for (int i = 0; i < CountEqual.Length; i++)
+{
+    CountEqual[i] = new Random().Next(-100, 100);
+    if (CountEqual[i] % 2 == 1)
+    {
+        count += CountEqual[i];
+
+    }
+    //Console.WriteLine ("count " + count);
+    Console.Write(CountEqual[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Cуммa элементов, стоящих на нечётных позициях   = " + count);
